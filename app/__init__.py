fc7ff48994a10c217dab8c2d3ba482365fffa1ea
shensha231 <sornikovaevelina72@gmail.com>
2026-02-15 17:35:04 +0400

    Remove db.create_all() to fix migration conflict with Alembic

--- app/__init__.py
+++ app/__init__.py
@@ -52,9 +52,5 @@ def create_app():
         if current_user.is_authenticated:
             return redirect(url_for("transactions.dashboard"))
         return render_template("index.html")
-
-    # Создание таблиц БД при первом запуске
-    with app.app_context():
-        db.create_all()
     
     return app
